a3dc8c53235162bce9425383032285717d54cb97
Vishal Ramesh <vishalramesh50@gmail.com>
2020-02-09 19:34:44 -0500

    Working version

--- main.py
+++ main.py
@@ -1,33 +1,38 @@
 import os
 import requests
 import time
+from datetime import datetime, timedelta
 from dotenv import load_dotenv
+from pytz import timezone
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.remote.webelement import WebElement
-from typing import List, Dict
+from typing import List, Dict, Optional
 
 load_dotenv()
 
-SPOTIFY_USERNAME: str = os.getenv("SPOTIFY_USERNAME")
-SPOTIFY_PASSWORD: str = os.getenv("SPOTIFY_PASSWORD")
+SPOTIFY_USERNAME: Optional[str] = os.getenv("SPOTIFY_USERNAME")
+SPOTIFY_PASSWORD: Optional[str] = os.getenv("SPOTIFY_PASSWORD")
 
 print("Starting up Chromium Browser...")
 chrome_options = Options()
 chrome_options.add_argument("--headless")
-driver = webdriver.Chrome(executable_path="./chromedriver", chrome_options=chrome_options)
+driver = webdriver.Chrome(
+    executable_path="./chromedriver", chrome_options=chrome_options
+)
 driver.get("https://developer.spotify.com/console/post-playlists/")
 
 
 # -------------------------- SELECT SCOPES ----------------------------
 print("Clicking on Get Token Button...")
-get_token_button: WebElement = driver.find_element_by_xpath("//button[text()='Get Token']").click()
+get_token_button: WebElement = driver.find_element_by_xpath(
+    "//button[text()='Get Token']"
+).click()
 time.sleep(1)
 print("Selecting scopes...")
 scopes: List[WebElement] = driver.find_elements_by_class_name("control-indicator")
 playlist_modify_public_scope: WebElement = scopes[0].click()
-playlist_modify_public_scope: WebElement = scopes[1].click()
+playlist_modify_public_scope = scopes[1].click()
 driver.find_element_by_id("oauthRequestToken").click()
 time.sleep(1)
 # -------------------------- SELECT SCOPES ----------------------------
@@ -52,17 +57,36 @@ print(f"Token Is: {token}")
 discover_weekly_playlist_id: str = "37i9dQZEVXcJN8VxgViEXj"
 
 # get all the uris for each track from the Discover Weekly Playlist
-discover_weekly_uri_response: requests.Response = requests.get(f"https://api.spotify.com/v1/playlists/{discover_weekly_playlist_id}/tracks?fields=items(track(uri))", headers={"Authorization": f"Bearer {token}"})
-all_uris: List[Dict[str, Dict[str, str]]] = discover_weekly_uri_response.json()["items"]
-print(all_uris)
-uris: List[str] = []
-for element in all_uris:
-    uris.append(element['track']['uri'])
-uris = ','.join(uris)
-# add all tracks from Discover Weekly to New Playlist
-result = requests.post(f"https://api.spotify.com/v1/playlists/498pnJiIsrER6WlakNT212/tracks?uris={uris}", headers={"Authorization": f"Bearer {token}"})
+discover_weekly_uri_response: requests.Response = requests.get(
+    f"https://api.spotify.com/v1/playlists/{discover_weekly_playlist_id}/tracks?fields=items(track(uri))",
+    headers={"Authorization": f"Bearer {token}"},
+)
+uri_data: List[Dict[str, Dict[str, str]]] = discover_weekly_uri_response.json()["items"]
+uri_items: List[str] = []
+for uri in uri_data:
+    uri_items.append(uri["track"]["uri"])
+uris: str = ",".join(uri_items)
 
-body = "{\"name\": \"My Personal Discover Weekly\"}"
-result = requests.put("https://api.spotify.com/v1/playlists/498pnJiIsrER6WlakNT212", data=body, headers={"Authorization": f"Bearer {token}"})
-print(result.status_code)
-print(result.text)
+start_date: datetime = datetime.now(timezone("US/Eastern"))  # EST timezone
+end_date: datetime = start_date + timedelta(days=7)
+name: str = f"{start_date.strftime('%-m/%-d')}-{end_date.strftime('%-m/%-d')}"
+date_string: str = f"{start_date.strftime('%b %-d')} - {end_date.strftime('%b %-d, %Y')}"
+body: str = f'{{"name": "{name} Weekly Playlist", "description": "Discover Weekly Playlist for {date_string}", "public": false}}'
+# creates new playlist
+result: requests.Response = requests.post(
+    f"https://api.spotify.com/v1/users/{SPOTIFY_USERNAME}/playlists",
+    data=body,
+    headers={"Authorization": f"Bearer {token}"},
+)
+result_data = result.json()
+
+# add all tracks from Discover Weekly to New Playlist
+new_playlist_id: str = result.json()["id"]
+result = requests.post(
+    f"https://api.spotify.com/v1/playlists/{new_playlist_id}/tracks?uris={uris}",
+    headers={"Authorization": f"Bearer {token}"},
+)
+
+playlist_name = result_data["name"]
+playlist_url = result_data["external_urls"]["spotify"]
+print(f'"{playlist_name}" available at "{playlist_url}"')
