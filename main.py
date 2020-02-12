704f07147a3fb1441cb056156806283691b18593
Vishal Ramesh <vishalramesh50@gmail.com>
2020-02-12 01:00:23 -0500

    Logging, Scopes, Generify Weekly Playlist ID, Quit Browser
    Switch to logging, skip selecting scopes, generify getting weekly playlist id, actually quit browser when done with process

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import logging
 import requests
 import time
 from datetime import datetime, timedelta
@@ -10,6 +11,7 @@ from selenium.webdriver.remote.webelement import WebElement
 from typing import List, Dict, Optional
 
 load_dotenv()
+logging.basicConfig(level=logging.INFO)
 
 SPOTIFY_USERNAME: Optional[str] = os.getenv("SPOTIFY_USERNAME")
 SPOTIFY_PASSWORD: Optional[str] = os.getenv("SPOTIFY_PASSWORD")
@@ -17,7 +19,7 @@ GOOGLE_CHROME_BIN: Optional[str] = os.getenv("GOOGLE_CHROME_BIN")
 CHROMEDRIVER_PATH: str = os.getenv("CHROMEDRIVER_PATH", "./chromedriver")
 
 
-print("Starting up Chromium Browser...")
+logging.info("Starting up Chromium Browser...")
 chrome_options = Options()
 if GOOGLE_CHROME_BIN:
     chrome_options.binary_location = GOOGLE_CHROME_BIN
@@ -30,21 +32,18 @@ driver.get("https://developer.spotify.com/console/post-playlists/")
 
 
 # -------------------------- SELECT SCOPES ----------------------------
-print("Clicking on Get Token Button...")
+logging.info("Clicking on Get Token Button...")
 get_token_button: WebElement = driver.find_element_by_xpath(
     "//button[text()='Get Token']"
 ).click()
 time.sleep(1)
-print("Selecting scopes...")
-scopes: List[WebElement] = driver.find_elements_by_class_name("control-indicator")
-playlist_modify_public_scope: WebElement = scopes[0].click()
-playlist_modify_public_scope = scopes[1].click()
+logging.info("Clicking on Request Token Button...")
 driver.find_element_by_id("oauthRequestToken").click()
 time.sleep(1)
 # -------------------------- SELECT SCOPES ----------------------------
 
 # -------------------------- SPOTIFY SIGN IN ---------------------------
-print("Signing into Spotify...")
+logging.info("Signing into Spotify...")
 username_field: WebElement = driver.find_element_by_id("login-username")
 username_field.send_keys(SPOTIFY_USERNAME)
 password_field: WebElement = driver.find_element_by_id("login-password")
@@ -54,20 +53,26 @@ time.sleep(2)
 # -------------------------- SPOTIFY SIGN IN ---------------------------
 
 # --------------------- RETRIEVE GENERATED TOKEN -----------------------
-print("Getting Shiny New Token...")
+logging.info("Getting Shiny New Token...")
 oauth_input: WebElement = driver.find_element_by_id("oauth-input")
 token: str = oauth_input.get_attribute("value")
-print(f"Token Is: {token}")
+logging.info(f"Token Is: {token}")
 # --------------------- RETRIEVE GENERATED TOKEN -----------------------
 
-discover_weekly_playlist_id: str = "37i9dQZEVXcJN8VxgViEXj"
+headers: Dict[str, str] = {"Authorization": f"Bearer {token}"}
+# Get Discover Weekly Playlist Data
+dw_playlist_result: requests.Response = requests.get(
+    "https://api.spotify.com/v1/search?q=Discover Weekly&type=playlist&limit=1",
+    headers=headers,
+)
+dw_playlist_id: str = dw_playlist_result.json()["playlists"]["items"][0]["id"]
 
 # get all the uris for each track from the Discover Weekly Playlist
-discover_weekly_uri_response: requests.Response = requests.get(
-    f"https://api.spotify.com/v1/playlists/{discover_weekly_playlist_id}/tracks?fields=items(track(uri))",
-    headers={"Authorization": f"Bearer {token}"},
+dw_uri_response: requests.Response = requests.get(
+    f"https://api.spotify.com/v1/playlists/{dw_playlist_id}/tracks?fields=items(track(uri))",
+    headers=headers,
 )
-uri_data: List[Dict[str, Dict[str, str]]] = discover_weekly_uri_response.json()["items"]
+uri_data: List[Dict[str, Dict[str, str]]] = dw_uri_response.json()["items"]
 uri_items: List[str] = []
 for uri in uri_data:
     uri_items.append(uri["track"]["uri"])
@@ -75,24 +80,29 @@ uris: str = ",".join(uri_items)
 
 start_date: datetime = datetime.now(timezone("US/Eastern"))  # EST timezone
 end_date: datetime = start_date + timedelta(days=7)
+# Playlist Name in the format of mm/dd-mm/dd with no digit padding (Ex: 1/1-1/7)
 name: str = f"{start_date.strftime('%-m/%-d')}-{end_date.strftime('%-m/%-d')}"
+# Date string in the form of Month dd - Month dd, YYYY (Ex: Jan 1 - Jan 7, 2020)
 date_string: str = f"{start_date.strftime('%b %-d')} - {end_date.strftime('%b %-d, %Y')}"
 body: str = f'{{"name": "{name} Weekly Playlist", "description": "Discover Weekly Playlist for {date_string}", "public": false}}'
-# creates new playlist
-result: requests.Response = requests.post(
+# creates new private playlist
+new_playlist_reponse: requests.Response = requests.post(
     f"https://api.spotify.com/v1/users/{SPOTIFY_USERNAME}/playlists",
     data=body,
-    headers={"Authorization": f"Bearer {token}"},
+    headers=headers,
 )
-result_data = result.json()
+result_data = new_playlist_reponse.json()
 
 # add all tracks from Discover Weekly to New Playlist
-new_playlist_id: str = result.json()["id"]
-result = requests.post(
+new_playlist_id: str = new_playlist_reponse.json()["id"]
+requests.post(
     f"https://api.spotify.com/v1/playlists/{new_playlist_id}/tracks?uris={uris}",
-    headers={"Authorization": f"Bearer {token}"},
+    headers=headers,
 )
 
-playlist_name = result_data["name"]
-playlist_url = result_data["external_urls"]["spotify"]
-print(f'"{playlist_name}" available at "{playlist_url}"')
+# close the browser session
+driver.quit()
+
+playlist_name: str = result_data["name"]
+playlist_url: str = result_data["external_urls"]["spotify"]
+logging.info(f'"{playlist_name}" available at "{playlist_url}"')
